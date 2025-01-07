6bb63a3a0a1a2ee9b212084b3e37ef3d32b68c33
Julio González <juliogtrenard@gmail.com>
2025-01-07 18:02:01 +0100

    ADD conexion a la base de datos

--- src/main/java/es/juliogtrenard/jaspersupermercado/SupermercadoApplication.java
+++ src/main/java/es/juliogtrenard/jaspersupermercado/SupermercadoApplication.java
@@ -6,16 +6,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
+/**
+ * Clase que muestra una aplicacion con reportes de un supermercado
+ */
 public class SupermercadoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SupermercadoApplication.class.getResource("/es/juliogtrenard/jaspersupermercado/fxml/supermercado.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("REPORTES");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * El punto de entrada de la aplicacion
+     * @param args argumentos de linea de comandos
+     */
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/es/juliogtrenard/jaspersupermercado/db/DBConnect.java
+++ src/main/java/es/juliogtrenard/jaspersupermercado/db/DBConnect.java
@@ -1,4 +1,67 @@
 package es.juliogtrenard.jaspersupermercado.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Clase que maneja la conexion a la base de datos
+ */
 public class DBConnect {
+    /**
+     * La conexion
+     */
+    private static Connection connection;
+
+    /**
+     * Constructor
+     *
+     * @throws SQLException the SQL exception
+     */
+    public DBConnect() throws SQLException {
+        Properties connConfig =loadProperties() ;
+        String url=connConfig.getProperty("dburl");
+        connection = DriverManager.getConnection(url, connConfig);
+        connection.setAutoCommit(true);
+        connection.setAutoCommit(true);
+    }
+
+    /**
+     * Getter de connection.
+     *
+     * @return the connection
+     */
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * Cerrar la conexion.
+     *
+     * @return the connection
+     * @throws SQLException the SQL exception
+     */
+    public Connection CloseConexion() throws SQLException{
+        connection.close();
+        return connection;
+    }
+
+    /**
+     * Carga las propiedades del archivo db.properties.
+     *
+     * @return the properties
+     */
+    public static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("configuration.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
